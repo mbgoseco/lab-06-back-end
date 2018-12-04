038d185533ddc32da32cef3ae7493dd635da1339
mbgoseco <mbgoseco@gmail.com>
2018-12-04 10:11:23 -0800

    server.js and npm setup

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
